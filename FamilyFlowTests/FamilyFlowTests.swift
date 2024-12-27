ef78462298dcf4e4146a2e7488ee79a8ce9d6f25
Drozdnik <drozdnik@list.ru>
2024-12-28 00:58:28 +0300

    Initial settings for xcodegen

---
+++ FamilyFlowTests/FamilyFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyFlowTests.swift
+//  FamilyFlowTests
+//
+//  Created by Михаил  on 27.12.2024.
+//
+
+import Testing
+@testable import FamilyFlow
+
+struct FamilyFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

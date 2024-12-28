fb942222e6674658cb54100d12f7148a62cbc0f5
Dmitry <dmitry.korchagin.s@gmail.com>
2024-12-28 23:34:57 +0300

    Add spm FFCore Package

---
+++ FFCore/Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FFCore",
+    products: [
+        .library(name: "FFCore", targets: ["FFCore"]),
+    ],
+    targets: [
+        .target(name: "FFCore"),
+    ]
+)

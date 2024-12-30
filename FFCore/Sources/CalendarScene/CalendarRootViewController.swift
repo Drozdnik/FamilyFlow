0d6916168a00a5a8c848ea4b2824479bd3d979c8
Dmitry <dmitry.korchagin.s@gmail.com>
2024-12-30 22:15:22 +0300

    Create main TabbarController

---
+++ FFCore/Sources/CalendarScene/CalendarRootViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+final class CalendarRootViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
---
+++ FFCore/Sources/OverviewScene/OverviewRootViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+final class OverviewRootViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .secondarySystemBackground
+    }
+}
